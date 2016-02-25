1e8bfd06ba73367d353aed9209b2efe49be67615
Samy Lanka <lankas@sas.upenn.edu>
2016-02-25 00:47:19 -0500

    created client and garage apps

--- ezpark/ezpark/settings.py
+++ ezpark/ezpark/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'web_server', 
+    'client',
+    'garage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
