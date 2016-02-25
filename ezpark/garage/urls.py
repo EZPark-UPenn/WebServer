0f902cf078c1ce9ac907fa202958c8d81a01a317
CONSTANZA <cfigu@seas.upenn.edu>
2016-02-25 01:31:40 -0500

    Added garage / files

---
+++ ezpark/garage/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+    url(r'^home/', views.home),
+    url(r'^analytics/', views.analytics),
+    url(r'^payment/', views.payment),
+    url(r'^history/', views.history),
+]
--- ezpark/garage/views.py
+++ ezpark/garage/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("This is the garage home page")
+
+def analytics(request):
+    return HttpResponse("This is the garage analytics page")
+
+def payment(request):
+    return HttpResponse("This is the garage payment page")
+
+def history(request):
+    return HttpResponse("This is the garage history page")
