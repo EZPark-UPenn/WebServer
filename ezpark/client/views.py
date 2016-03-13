dc7899bfd567483e3135480d44192d5890920ff9
Utkarsh Shah <utkarshshah007@gmail.com>
2016-03-13 01:23:53 -0500

    client frontend, hardcoded

--- ezpark/client/views.py
+++ ezpark/client/views.py
@@ -3,19 +3,23 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the client home.")
+	return render(request, 'client/index.html')
+    # return HttpResponse("Hello, world. You're at the client home.")
 
 def cars(request):
-    return HttpResponse("Hello, world. You're at the client's cars.")
+	return render(request, 'client/cars-overview.html')
+    # return HttpResponse("Hello, world. You're at the client's cars.")
 
 def register_car(request):
-    return HttpResponse("Hello, world. You're at the client's register_car page.")
+	return render(request, 'client/register-car.html')
+    # return HttpResponse("Hello, world. You're at the client's register_car page.")
 
 def car_history(request):
     return HttpResponse("Hello, world. You're at the client's car's history.")
 
 def client_history(request):
-    return HttpResponse("Hello, world. You're at the client's overall history.")
+	return render(request, 'client/client-history.html')
+    # return HttpResponse("Hello, world. You're at the client's overall history.")
 
 def client_payment(request):
     return HttpResponse("Hello, world. You're at the client's payment method.")
