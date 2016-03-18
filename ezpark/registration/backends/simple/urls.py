044f33c1df81f80d0aaa010879195ab815fa667d
Samy Lanka <lankas@sas.upenn.edu>
2016-03-18 14:21:12 -0400

    registration to home page for both client and garage

--- ezpark/registration/backends/simple/urls.py
+++ ezpark/registration/backends/simple/urls.py
@@ -7,13 +7,15 @@ workflow.
 from django.conf.urls import include, url
 from django.views.generic.base import TemplateView
 
-from .views import RegistrationView
-
+from registration.views import ClientRegistrationView, GarageRegistrationView
 
 urlpatterns = [
-    url(r'^register/$',
-        RegistrationView.as_view(),
-        name='registration_register'),
+    url(r'^register-client/$',
+        ClientRegistrationView.as_view(),
+        name='registration_register_client'),
+    url(r'^register-garage/$',
+        GarageRegistrationView.as_view(),
+        name='registration_register_garage'),
     url(r'^register/closed/$',
         TemplateView.as_view(
             template_name='registration/registration_closed.html'
